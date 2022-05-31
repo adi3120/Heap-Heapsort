3b3c958c537029a08a202fb7da42f073236c8584
adi3120 <aditya.yadav.cs.20@ggits.net>
2022-05-31 14:12:35 +0530

    Removed insertion and delete_max functions coz I only need Heapify(), adjust() and heap_sort()

--- heap.cpp
+++ heap.cpp
@@ -2,15 +2,6 @@
 #include<vector>
 using namespace std;
 
-void insert(vector<int> & heap,int item){
-	heap.push_back(item);
-	int i=heap.size()-1;
-	while(i>1 && heap[(i-1)/2]<heap[i]){
-		swap(heap[i],heap[(i-1)/2]);
-		i=(i-1)/2;
-	}
-}
-
 void adjust(int arr[],int n,int i){
 	int largest_index=i;
 
@@ -29,28 +20,6 @@ void adjust(int arr[],int n,int i){
 	}
 }
 
-int* delete_max(int arr[],int size){
-	int * arr_new=new int[size-1];
-
-	if(size==0){
-		cout<<"Empty Heap"<<endl;
-	}
-	else{
-		int x=arr[0];
-		arr[0]=arr[size-1];
-		copy(arr,arr+size-1,arr_new);
-		adjust(arr_new,size-1,0);
-	}
-	return arr_new;
-
-}
-
-void print_heap(int heap[],int size){
-	for(int i=0;i<size;i++){
-		cout<<heap[i]<<" ";
-	}
-	cout<<endl;
-}
 
 void heapify(int arr[],int size){
 	for(int i=size/2;i>0;i--){
@@ -67,19 +36,30 @@ void heap_sort(int arr[],int size){
 	}
 }
 
+void print_array(int arr[],int size){
+	for(int i=0;i<size;i++){
+		cout<<arr[i]<<" ";
+	}
+	cout<<endl;
+}
+
 int main(){
 
 	int arr[]={100,119,118,171,112,151,132};
 	int size=sizeof arr/sizeof arr[0];
 
-	print_heap(arr,size);
+	cout<<"Before Heap Sort : ";
+
+	print_array(arr,size);
 
 	heap_sort(arr,size);
 
 	//heapify the array
 	//swap first and last element
 	//adjust heap leaving the last elements
-	
-	print_heap(arr,size);
+
+	cout<<"After Heap Sort : ";
+
+	print_array(arr,size);
 
 }
